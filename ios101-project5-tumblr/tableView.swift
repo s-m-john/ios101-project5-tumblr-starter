23a01a89612f7fb515dc9a26189cad08803787fb
s-m-john <smc_john@hotmail.com>
2023-10-20 17:27:52 -0400

    STEP 2 Completed

---
+++ ios101-project5-tumblr/tableView.swift
@@ -0,0 +1,29 @@
+//
+//  tableView.swift
+//  ios101-project5-tumblr
+//
+//  Created by Sophia John on 10/19/23.
+//
+
+import UIKit
+
+class tableView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
